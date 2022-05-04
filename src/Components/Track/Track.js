516510ed3ff150fa2b55217c3c2483079a384551
Kenneth <kenrathbun1@gmail.com>
2022-05-03 19:05:08 -0700

    Steps 1 - 61 of Codecademy project

---
+++ src/Components/Track/Track.js
@@ -0,0 +1,44 @@
+import React from "react";
+import "./Track.css";
+
+export default class Track extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.addTrack = this.addTrack.bind(this);
+    this.removeTrack = this.removeTrack.bind(this)
+  }
+  
+  renderAction() {
+    return this.props.isRemoval ? 
+            <button className="Track-action"
+                    onClick={this.removeTrack}>-</button> :
+            <button className="Track-action"
+                    onClick={this.addTrack}>+</button>;
+  }
+
+  addTrack() {
+    let track = this.props.track;
+    this.props.onAdd(track);
+  }
+
+  removeTrack() {
+    let track = this.props.track;
+    this.props.onRemove(track);
+  }
+
+  render() {
+    return (
+      <div className="Track">
+        <div className="Track-information">
+          <h3>{this.props.track.name}</h3>
+          <p>
+            {this.props.track.artist} | {this.props.track.album}
+          </p>
+        </div>
+        {/* <button class="Track-action"><!-- + or - will go here --></button> */}
+        {this.renderAction()}
+      </div>
+    );
+  }
+}
